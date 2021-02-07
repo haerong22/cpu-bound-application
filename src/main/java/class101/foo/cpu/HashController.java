0c50eb866b1e851f0588368dc91e49964acfe01f
kim <haerong22@gmail.com>
2021-02-08 02:41:18 +0900

    change message

--- src/main/java/class101/foo/cpu/HashController.java
+++ src/main/java/class101/foo/cpu/HashController.java
@@ -21,7 +21,7 @@ public class HashController {
 
     @RequestMapping("/hello")
     public String hello() {
-        return "hello deploy test test test";
+        return "hello deploy test test test 222";
     }
 
     private String getMD5Digest(String input) throws NoSuchAlgorithmException {
